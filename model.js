dd5a6ebc76035159ef59430a1d5d84833333740d
Gibran Gonzalez <hector.gibran@hotmail.com>
2019-11-13 14:16:27 -0600

    Adds model schema and quizPost method for model

--- model.js
+++ model.js
@@ -1,10 +1,35 @@
 let mongoose = require('mongoose'); //dependency to connect Node with Mongo
 mongoose.Promise = global.Promise; // 
 
-let studentSchema = mongoose.Schema({ //Schema is a meethod to build a schema, you pass in an object with the attributes the schema will have
-	firstName : { type : String }, //
-	lastName : { type : String },
-	id : { type : Number, required : true }
+let quizSchema = mongoose.Schema({ //Schema is a meethod to build a schema, you pass in an object with the attributes the schema will have
+	quizQuestions : { type : Array }, //
+	quizTags : { type : Array },
+	quizTitle : { type : String },
+	user : { type : String }
 });
 
-let Student = mongoose.model('Student', studentSchema); //Model is a method to create a collection
+let Quiz = mongoose.model('Quiz', quizSchema); //Model is a method to create a collection
+
+let QuizList = {
+	getAll: function(){
+		return Quiz.find()
+			.then(quizzes => {
+				return quizzes;
+			})
+			.catch(error => {
+				throw Error(error);
+			})
+	},
+	post : function(newQuiz) {
+		return Quiz.create(newQuiz)
+			.then(quiz => {
+				return quiz;
+			})
+			.catch(error => {
+				throw Error(error);
+			})
+	}
+}
+
+module.exports = {QuizList};
+
--- server.js
+++ server.js
@@ -3,13 +3,37 @@ let morgan = require("morgan");
 let bodyParser = require("body-parser");
 let app = express();
 let jsonParser = bodyParser.json();
+const {DATABASE_URL, PORT} = require('./config')
 
 let mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
+let { QuizList } = require('./model');
 
 app.use(express.static('public'));
 app.use(morgan("dev"));
 
+app.post("/api/postQuiz", jsonParser, (req, res) => {
+
+	let newQuiz = req.body.quiz;
+
+	QuizList.post(newQuiz)
+		.then(newQuiz => {
+			return res.status(201).json({
+				message: "Quiz added to the list", 
+				status: 201, 
+				quiz : newQuiz
+			});
+		})
+		.catch(error => {
+			res.statusMessage = "Something went wrong with the DB. Try again later.";
+			return res.status(500).json({
+				message: "Something went wrong with the DB. Try again later.",
+				status: 500
+			});
+		});
+});
+
+let server;
 function runServer(port, databaseUrl) { //function to run when the server starts
 	return new Promise((resolve, reject) => {
 		mongoose.connect( databaseUrl, error => { //the 'error' parameter is only going to be holding
@@ -25,8 +49,27 @@ function runServer(port, databaseUrl) { //function to run when the server starts
 	})
 }
 
-runServer(8080, "mongodb://localhost/QuizOnDB")
+function closeServer(){
+	 return mongoose.disconnect()
+	 .then(() => {
+	 	return new Promise((resolve, reject) => {
+	 		console.log('Closing the server');
+			server.close( err => {
+				 if (err){
+				 	return reject(err);
+				 }
+				 else{
+				 	resolve();
+				 }
+			 });
+	 	});
+	 });
+}
+
+runServer(PORT, DATABASE_URL)
 	.catch(error => {
 		console.log(error);
 	});
+
+module.exports = {app, runServer, closeServer};
 
